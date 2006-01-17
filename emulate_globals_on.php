6175afbc4e921458b83eebaa539d730c256c7463
Neil Williams <linux@codehelp.co.uk>
2006-01-17 15:28:50 +0000

    patching english files for GLOBALS, adding missing graphic
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@12823 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ emulate_globals_on.php
@@ -0,0 +1,13 @@
+<?php
+ // Emulate register_globals on
+ if (!ini_get('register_globals')) {
+    $superglobals = array($_SERVER, $_ENV,
+        $_FILES, $_COOKIE, $_POST, $_GET);
+    if (isset($_SESSION)) {
+        array_unshift($superglobals, $_SESSION);
+    }
+    foreach ($superglobals as $superglobal) {
+        extract($superglobal, EXTR_SKIP);
+    }
+ }
+?>
