46d9c68fa88972406c8e9d3c6d0df1542d8f20a4
Neil Williams <linux@codehelp.co.uk>
2006-02-11 20:03:07 +0000

    handling content-type
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@13214 57a11ea4-9604-0410-9ed3-97b8803252fd

--- emulate_globals_on.php
+++ emulate_globals_on.php
@@ -21,12 +21,14 @@ include($toppath."/accept-to-gettext.inc");
 # Set the language as 'en_GB' for testing
 # note that gettext in php requires the entire
 # string in one line, one quote.
-$langs=array('nl_BE.ISO-8859-15','nl_BE.UTF-8','en_GB.UTF-8','en_GB','en_US.UTF-8');
+$langs=array('nl_BE.ISO-8859-15','nl_BE.UTF-8','en_GB.UTF-8','en_GB','en_US.UTF-8', 'en_US');
 $locale=al2gt($langs, 'text/html');
+# if/when we support non-Latin-1 languages, add some logic to modify this line.
+$mimeline = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-15\">\n";
 # allow user override.
 $language = $_GET["lang"];
 if(isset($language)) { $locale = $language; }
-if($locale == "") { $locale = 'en_GB'; }
+if($locale == "") { $locale = 'en_US'; }
 putenv("LANG=$locale");
 setlocale(LC_MESSAGES, $locale);
 $domain = "gnucash-htdocs";
