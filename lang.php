ff02b14664a5be638bb7b9bd4a331d1555499beb
Joshua Sled <jsled@asynchronous.org>
2006-05-11 00:49:33 +0000

    try both envvars.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@13999 57a11ea4-9604-0410-9ed3-97b8803252fd

--- lang.php
+++ lang.php
@@ -4,6 +4,7 @@ $language = $_GET["lang"];
 if(isset($language)) { $locale = $language; }
 if($locale == "") { $locale = "en_US"; }
 putenv("LANG=$locale");
+putenv("LANGUAGE=$locale");
 setlocale(LC_MESSAGES, $locale);
 $domain = "gnucash-htdocs";
 bindtextdomain($domain, $top_dir);
