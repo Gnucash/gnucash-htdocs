b5658a6ce8b5ade5381944b04885ebc8dc70584a
Joshua Sled <jsled@asynchronous.org>
2006-05-11 01:20:27 +0000

    try LC_ALL for www.gnucash.org :p
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@14003 57a11ea4-9604-0410-9ed3-97b8803252fd

--- lang.php
+++ lang.php
@@ -1,11 +1,11 @@
 <?php
 # allow user override.
 $language = $_GET["lang"];
-if(isset($language)) { $locale = $language; }
-if($locale == "") { $locale = "en_US"; }
+if (isset($language)) { $locale = $language; }
+if ($locale == "") { $locale = "en_US"; }
 putenv("LANG=$locale");
 putenv("LANGUAGE=$locale");
-setlocale(LC_MESSAGES, $locale);
+setlocale(LC_ALL, $locale);
 $domain = "gnucash-htdocs";
 bindtextdomain($domain, $top_dir);
 textdomain($domain);
