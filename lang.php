2bd370f9e716369d03c3d77730878e25f3e0ae29
Joshua Sled <jsled@asynchronous.org>
2006-05-18 01:46:31 +0000

    try something else...
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@14120 57a11ea4-9604-0410-9ed3-97b8803252fd

--- lang.php
+++ lang.php
@@ -5,10 +5,10 @@ if (isset($language)) { $locale = $language; }
 if ($locale == "") { $locale = "en_US"; }
 putenv("LANG=$locale");
 putenv("LANGUAGE=$locale");
-$locale_res = setlocale(LC_ALL, $locale);
+$locale_res = setlocale(LC_ALL, "");
 $domain = "gnucash-htdocs";
 $dir_res = bindtextdomain($domain, $top_dir);
 textdomain($domain);
-if ($local_res == false) { $local_res = 'false'; }
+if (!$local_res) { $local_res = 'invalid'; }
 echo ("<!-- $lang , locale_res [$locale_res] , dir_res $dir_res -->\n");
 ?>
