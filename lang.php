76869410b1d334b1c8de5d59c8b24b3a18ef4157
Derek Atkins <derek@ihtfp.com>
2006-05-31 00:20:36 +0000

    Add es, nb, and pl to the top-level translation menu. Also add the appropriate news transaction directories so news-script works.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@14272 57a11ea4-9604-0410-9ed3-97b8803252fd

--- lang.php
+++ lang.php
@@ -7,7 +7,7 @@ putenv("LANG=$locale");
 putenv("LANGUAGE=$locale");
 $locale_res = setlocale(LC_ALL, "");
 $domain = "gnucash-htdocs";
-if (! isset($text_dir)) { $test_dir = $top_dir; }
+if (! isset($text_dir)) { $text_dir = $top_dir; }
 $dir_res = bindtextdomain($domain, $text_dir);
 textdomain($domain);
 echo ("<!-- $lang , locale_res [$locale_res] , dir_res $dir_res -->\n");
