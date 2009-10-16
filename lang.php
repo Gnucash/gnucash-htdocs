0d776371342f1d744ac017e85d4e9ddb30399c30
Christian Stimming <stimming@tuhh.de>
2009-10-16 20:04:24 +0000

    Request the i18nized to be in UTF-8.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@18382 57a11ea4-9604-0410-9ed3-97b8803252fd

--- lang.php
+++ lang.php
@@ -9,6 +9,10 @@ $locale_res = setlocale(LC_ALL, "");
 $domain = "gnucash-htdocs";
 if (! isset($text_dir)) { $text_dir = $top_dir; }
 $dir_res = bindtextdomain($domain, $text_dir);
+
+# Also specify that we want to receive the translated strings as UTF-8
+bind_textdomain_codeset($domain, 'UTF-8');
+
 textdomain($domain);
 echo ("<!-- $lang , locale_res [$locale_res] , dir_res $dir_res -->\n");
 ?>
