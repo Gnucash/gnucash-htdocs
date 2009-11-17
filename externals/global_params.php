2620e6b05bfdda7106cf8e4e09c4cda2be4f2cc6
Christian Stimming <stimming@tuhh.de>
2009-11-17 21:18:34 +0000

    Website update by Geert Janssens
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@18421 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ externals/global_params.php
@@ -0,0 +1,23 @@
+<?php
+     $latest_stable="2.2.9";
+     $latest_unstable="2.3.7";
+     
+     $stable_path="http://sourceforge.net/projects/gnucash/files/gnucash%20(stable)/";
+     $latest_stable_path=$stable_path.$latest_stable."/";
+     
+     $latest_stable_win="gnucash-".$latest_stable."-setup.exe";
+     $latest_stable_mac_intel="Gnucash-Intel-".$latest_stable.".4.dmg";
+     $latest_stable_mac_ppc="Gnucash-PPC-".$latest_stable.".3.dmg";
+     $latest_stable_source="gnucash-".$latest_stable.".tar.bz2";
+     
+     $unstable_path="http://sourceforge.net/projects/gnucash/files/gnucash%20(unstable)/";
+     $latest_unstable_path=$unstable_path.$latest_unstable."/";
+     
+     $latest_unstable_win="gnucash-".$latest_unstable."-setup.exe";
+     $latest_unstable_mac_intel="Gnucash-Intel-".$latest_unstable.".dmg";
+     $latest_unstable_mac_ppc="Gnucash-PPC-".$latest_unstable.".dmg";
+     $latest_unstable_source="gnucash-".$latest_unstable.".tar.bz2";
+     
+     $download_path="http://downloads.sourceforge.net/sourceforge/gnucash/";
+     
+?>
