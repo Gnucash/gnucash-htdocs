7df55b806529141a6eea561bcdf92dd8f427871f
Geert Janssens <janssens-geert@telenet.be>
2010-02-17 08:53:13 +0000

    Update release info to 2.3.9 Also simplify the file structure and add some documentation
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@18665 57a11ea4-9604-0410-9ed3-97b8803252fd

--- externals/global_params.php
+++ externals/global_params.php
@@ -1,21 +1,35 @@
 <?php
-     $latest_stable="2.2.9";
-     $latest_unstable="2.3.8";
-     
+     // This section defines the most recent version numbers for all platforms.
+     // Each platform can define its own latest version or follow the default.
+     // Both a latest stable version and a latest unstable version are defined.
+     $latest_stable             = "2.2.9";
+     $latest_stable_win         = $latest_stable;
+     $latest_stable_mac_ppc     = $latest_stable.".4";
+     $latest_stable_mac_intel   = $latest_stable.".4";
+
+     $latest_unstable           = "2.3.9";
+     $latest_unstable_win       = $latest_unstable;
+     $latest_unstable_mac_ppc   = "2.3.9";
+     $latest_unstable_mac_intel = "2.3.9";
+
+     // The definitions below use the latest version definitions above
+     // to define several paths that will be used on several pages
+     // on the website.
+     // Please don't make changes below unless you know what you are doing.
      $stable_path="http://sourceforge.net/projects/gnucash/files/gnucash%20(stable)/";
      $latest_stable_path=$stable_path.$latest_stable."/";
      
-     $latest_stable_win="gnucash-".$latest_stable."-setup.exe";
-     $latest_stable_mac_intel="Gnucash-Intel-".$latest_stable.".4.dmg";
-     $latest_stable_mac_ppc="Gnucash-PPC-".$latest_stable.".4.dmg";
+     $latest_stable_win="gnucash-".$latest_stable_win."-setup.exe";
+     $latest_stable_mac_intel="Gnucash-Intel-".$latest_stable_mac_intel.".dmg";
+     $latest_stable_mac_ppc="Gnucash-PPC-".$latest_stable_mac_ppc.".dmg";
      $latest_stable_source="gnucash-".$latest_stable.".tar.bz2";
      
      $unstable_path="http://sourceforge.net/projects/gnucash/files/gnucash%20(unstable)/";
      $latest_unstable_path=$unstable_path.$latest_unstable."/";
      
-     $latest_unstable_win="gnucash-".$latest_unstable."-setup.exe";
-     $latest_unstable_mac_intel="Gnucash-Intel-".$latest_unstable.".dmg";
-     $latest_unstable_mac_ppc="Gnucash-PPC-".$latest_unstable.".dmg";
+     $latest_unstable_win="gnucash-".$latest_unstable_win."-setup.exe";
+     $latest_unstable_mac_intel="Gnucash-Intel-".$latest_unstable_mac_intel.".dmg";
+     $latest_unstable_mac_ppc="Gnucash-PPC-".$latest_unstable_mac_ppc.".dmg";
      $latest_unstable_source="gnucash-".$latest_unstable.".tar.bz2";
      
      $download_path="http://downloads.sourceforge.net/sourceforge/gnucash/";
