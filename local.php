c01654314de03266f7f9aaec72463084f89824fb
Geert Janssens <janssens-geert@telenet.be>
2010-05-08 15:36:41 +0000

    Disable debuggin information
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@19144 57a11ea4-9604-0410-9ed3-97b8803252fd

--- local.php
+++ local.php
@@ -1,6 +1,5 @@
 <?php
-echo ("<!-- LANG: $LANG, ENV_LANG: ".$_ENV['LANG']." , SCRIPT_NAME: ".$_SERVER['SCRIPT_NAME'].", PATH_INFO: ".$_SERVER['PATH_INFO']." -->\n");
-echo ("<!-- top_dir: $top_dir, me: ".__FILE__."-->\n");
+#echo ("<!-- top_dir: $top_dir, me: ".__FILE__."-->\n");
 $top_dir = ".";
 $home = $top_dir;
 $locale = "";
