283ad493e767fedd3ae531f44694b49fa6a943a2
Cristian Marchi <cri79@libero.it>
2011-09-21 09:17:34 +0000

    Fix a missing div for Google's +1 button
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/branches/beta@21278 57a11ea4-9604-0410-9ed3-97b8803252fd

--- news/news-script.php
+++ news/news-script.php
@@ -134,6 +134,7 @@ function emit_news($en_newspath, $lang_newspath, $oldnews)
     <img src="<?=$top_dir?>/images/icons/document.txt.gif" width="16" height="16" alt="[news]" />&nbsp;
     <a style="text-decoration:none;color:black;" onmouseover="this.style.backgroundColor='#cacaf7';" onmouseout="this.style.backgroundColor='#cacaf7';" name="<?=generate_anchor($key)?>"><?= $fa[0]; ?> - <b><?= $newsfile[$key] ?></b></a>
   </div>
+  </div>
   <div class="newsinner">
   <? for ($i=2; $i<$n; $i++)  {
           print $fa[$i];
