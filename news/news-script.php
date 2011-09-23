c33ba7e66e048c8a899f9f09b60eaa354a1e2364
Cristian Marchi <cri79@libero.it>
2011-09-23 16:45:38 +0000

    Some cosmetics.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/branches/beta@21307 57a11ea4-9604-0410-9ed3-97b8803252fd

--- news/news-script.php
+++ news/news-script.php
@@ -130,11 +130,11 @@ function emit_news($en_newspath, $lang_newspath, $oldnews)
 ?>
 <div class="newsborder">
   <div class="newsheader" style="display:inline-block;background-color:#CACAF7;width:100%">
-    <div style="float:left; max-width:80%;">
+    <div style="float:left;">
     <img src="<?=$top_dir?>/images/icons/document.txt.gif" width="16" height="16" alt="[news]" />&nbsp;
     <a style="text-decoration:none;color:black;" onmouseover="this.style.backgroundColor='#cacaf7';" onmouseout="this.style.backgroundColor='#cacaf7';" name="<?=generate_anchor($key)?>"><?= $fa[0]; ?> - <b><?= $newsfile[$key] ?></b></a>
   </div>
-<!-- the next div could be used to add a date on the right of the news' heading-->
+<!-- the next div could be used to add a date on the right of the news heading-->
     <div style="float:right;">
     </div>
 
