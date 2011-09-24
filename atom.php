ec29f3dac0ba1642760944bb411232426d75ef08
Cristian Marchi <cri79@libero.it>
2011-09-24 16:53:24 +0000

    Test again atom feed modifications just to understand how it works.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/branches/beta@21330 57a11ea4-9604-0410-9ed3-97b8803252fd

--- atom.php
+++ atom.php
@@ -34,47 +34,14 @@
   include("$top_dir/news/news-script.php");
 ?>
 
-<feed version="2.0" xmlns="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns="http://www.w3.org/2005/Atom">
   <channel>
-  <id>http://www.gnucash.org/atom.php</id>
-  <title>GnuCash News</title>
+    <title>GnuCash News</title>
   <description>DESCRIPTION</description>
   <generator>GnuCash htdocs/trunk/atom.php 1.0 &lt;gnucash-devel@gnucash.org&gt;</generator>
   <logo>http://www.gnucash.org/beta/images/icons/gnc-icon.png</logo>
   <link rel="alternate" href="http://www.gnucash.org/beta/" />
   <link rel="self" href="http://www.gnucash.org/beta/atom.php" />
-  <?php
-  $news_items = get_news($newsdir, $newsdir);
-  $news_items = array_slice($news_items, 0, $entry_count);
-  reset($news_items);
-  $most_recent = file(key($news_items));
-  $most_recent_update = chop($most_recent[1]);
-  ?>
-  <updated><?= date_convert_news_to_atom($most_recent_update) ?></updated>
 
-  <?php for (reset($news_items); $key = key($news_items); next($news_items))
-  {
-    $fa = file($key);
-    $n = count($fa);
-    $title = strip_tags(chop($fa[0]));
-    $update_date = chop($fa[1]);
-    ?>
-  <entry>
-    <id>urn:x-gnucash:news:<?= urlencode($key) ?></id>
-    <title><?= $title ?></title>
-    <link rel="alternate" href="http://www.gnucash.org/beta/#<?=generate_anchor($key);?>" />
-    <author>
-      <name>GnuCash Developers</name>
-      <email>gnucash-devel@gnucash.org</email>
-    </author>
-    <updated><?= date_convert_news_to_atom($update_date) ?></updated>
-    <description><![CDATA[
-        <? for ($i=2; $i<$n; $i++) {
-            print ($fa[$i]);
-        } ?>
-    ]]></content>
-  </entry>
-  <?php
-  } ?>
   </channel>
-</feed>
+</rss>
