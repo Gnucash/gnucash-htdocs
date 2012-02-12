989763696be182dd303ad176d5de10607db54cd6
John Ralls <jralls@ceridwen.us>
2012-02-12 20:45:34 +0000

    Release 2.4.10-1 for Microsoft Windows
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@22013 57a11ea4-9604-0410-9ed3-97b8803252fd

--- externals/global_params.php
+++ externals/global_params.php
@@ -3,12 +3,13 @@
      // Each platform can define its own latest version or follow the default.
      // Both a latest stable version and a latest unstable version are defined.
      $latest_stable             = "2.4.10";
-     $latest_stable_win         = $latest_stable;
- #    $latest_stable_mac_ppc     = $latest_stable."-2";
+     $latest_stable_win		= $latest_stable."-1";
+#     $latest_stable_win         = $latest_stable;
+#    $latest_stable_mac_ppc     = $latest_stable."-2";
      $latest_stable_mac_ppc     = $latest_stable;
  #    $latest_stable_mac_intel   = $latest_stable."-2";
      $latest_stable_mac_intel   = $latest_stable;
-
+ 
      $latest_unstable           = "2.3.17";
      $latest_unstable_win       = $latest_unstable;
      $latest_unstable_mac_ppc   = $latest_unstable;
