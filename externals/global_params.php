e8d7ad785b3209ef5ae7700dfedd729e7c6e8e02
John Ralls <jralls@ceridwen.us>
2013-04-07 16:11:18 +0000

    Release 2.5.0
    git-svn-id: svn+ssh://svn.gnucash.org/repo/htdocs/trunk@22881 57a11ea4-9604-0410-9ed3-97b8803252fd

--- externals/global_params.php
+++ externals/global_params.php
@@ -10,9 +10,9 @@
 #     $latest_stable_mac_intel   = $latest_stable."-2";
      $latest_stable_mac_intel   = $latest_stable;
  
-#     $latest_unstable           = "2.3.17";
+     $latest_unstable           = "2.5.0";
      // There's currently no unstable release being worked on, so disable it
-     $latest_unstable           = False;
+#     $latest_unstable           = False;
      $latest_unstable_win       = $latest_unstable;
      $latest_unstable_mac_ppc   = $latest_unstable;
      $latest_unstable_mac_intel = $latest_unstable;
