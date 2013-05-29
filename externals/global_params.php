6561ef4ede8dfcb8b2e4bd1f56cf62d7da77379b
Geert Janssens <janssens-geert@telenet.be>
2013-05-29 15:41:07 +0200

    Soft reset of tmp (master) to beta
    This is step two to make beta a clean copy of master again

--- externals/global_params.php
+++ externals/global_params.php
@@ -2,17 +2,17 @@
      // This section defines the most recent version numbers for all platforms.
      // Each platform can define its own latest version or follow the default.
      // Both a latest stable version and a latest unstable version are defined.
-     $latest_stable             = "2.4.11";
+     $latest_stable             = "2.4.13";
 #     $latest_stable_win		= $latest_stable."-1";
      $latest_stable_win         = $latest_stable;
-#    $latest_stable_mac_ppc     = $latest_stable."-2";
+#     $latest_stable_mac_ppc     = $latest_stable."-2";
      $latest_stable_mac_ppc     = $latest_stable;
- #    $latest_stable_mac_intel   = $latest_stable."-2";
+#     $latest_stable_mac_intel   = $latest_stable."-2";
      $latest_stable_mac_intel   = $latest_stable;
  
-#     $latest_unstable           = "2.3.17";
+     $latest_unstable           = "2.5.2";
      // There's currently no unstable release being worked on, so disable it
-     $latest_unstable           = False;
+#     $latest_unstable           = False;
      $latest_unstable_win       = $latest_unstable;
      $latest_unstable_mac_ppc   = $latest_unstable;
      $latest_unstable_mac_intel = $latest_unstable;
