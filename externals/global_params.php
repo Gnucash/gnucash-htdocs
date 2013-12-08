3487cf50cdfd0d5045c2bdb20349c8b0797f2377
John Ralls <jralls@ceridwen.us>
2013-12-08 13:22:38 -0800

    Set Intel unstable dmg to -2, reflecting uptdate

--- externals/global_params.php
+++ externals/global_params.php
@@ -15,7 +15,7 @@
 #     $latest_unstable           = False;
      $latest_unstable_win       = $latest_unstable;
      $latest_unstable_mac_ppc   = $latest_unstable."-1";
-     $latest_unstable_mac_intel = $latest_unstable."-1";
+     $latest_unstable_mac_intel = $latest_unstable."-2";
 
      // The definitions below use the latest version definitions above
      // to define several paths that will be used on several pages
