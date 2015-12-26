23d21050797b65327bfbdef9da48a289d989a97d
John Ralls <jralls@ceridwen.us>
2015-12-26 14:29:56 -0800

    Update PPC download link now that the dmg is finally ready.

--- externals/global_params.php
+++ externals/global_params.php
@@ -7,8 +7,7 @@
      $latest_stable             = $major_stable.".".$minor_stable;
      $latest_stable_win		= $latest_stable;
 #     $latest_stable_win         = $latest_stable."-1";
-#     $latest_stable_mac_ppc     = $latest_stable."-1";
-     $latest_stable_mac_ppc     = major_stable."9-1";
+     $latest_stable_mac_ppc     = $latest_stable."-1";
      $latest_stable_mac_intel   = $latest_stable."-1";
 
 #     $major_latest_unstable		="2.5";
