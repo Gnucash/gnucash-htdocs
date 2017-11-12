263588e4fef38a278def7f54aa062662c40ac4b5
Geert Janssens <geert@kobaltwit.be>
2017-11-12 09:28:11 +0100

    Show unstable releases on the downloads page

--- externals/global_params.php
+++ externals/global_params.php
@@ -17,7 +17,7 @@
      $minor_latest_unstable		="1"; 
      $latest_unstable           = $major_latest_unstable.".".$minor_latest_unstable;
      // There's currently no unstable release being worked on, so disable it
-     $latest_unstable           = False;
+#     $latest_unstable           = False;
      $latest_unstable_win       = $latest_unstable;
      $latest_unstable_mac_intel = $latest_unstable."-1";
 
