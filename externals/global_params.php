4ecc1a6d3b5b5cd32230e6b562c7f5d37a48c4cc
John Ralls <jralls@ceridwen.us>
2018-02-03 15:37:35 -0800

    Announce release of 2.7.4

--- externals/global_params.php
+++ externals/global_params.php
@@ -14,7 +14,7 @@
      $tarball                   = $latest_stable."-1";
      
      $major_latest_unstable		="2.7";
-     $minor_latest_unstable		="3";
+     $minor_latest_unstable		="4";
      $latest_unstable           = $major_latest_unstable.".".$minor_latest_unstable;
      // There's currently no unstable release being worked on, so disable it
 #     $latest_unstable           = False;
