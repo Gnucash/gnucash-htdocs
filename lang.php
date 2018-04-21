550b34787559068738c977aaba49e6d7a051ce06
fell <frank.h.ellenberger@gmail.com>
2018-04-21 14:52:39 +0200

    uncomment ?>

--- lang.php
+++ lang.php
@@ -97,5 +97,5 @@ T_bind_textdomain_codeset($domain, 'UTF-8');
 T_textdomain($domain);
 
 # Next line was disabled in order to have the definition of doctype on the first line of the html resulting page. In this way the fixed attribute of the branding GnuCash sign works in ie8
-/*echo ("<!-- $locale , locale_res [$locale_res] , dir_res $dir_res, lang_cookie [$lang_cookie] -->\n");?>*/
-
+/*echo ("<!-- $locale , locale_res [$locale_res] , dir_res $dir_res, lang_cookie [$lang_cookie] -->\n");*/
+?>
