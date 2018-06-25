a505f5cbce3dca4c0bef2f7e35e786d36fb8bd0b
John Ralls <jralls@ceridwen.us>
2018-06-24 20:31:54 -0700

    Announce release of 3.2.

--- externals/global_params.php
+++ externals/global_params.php
@@ -3,14 +3,14 @@
      // Each platform can define its own latest version or follow the default.
      // Both a latest stable version and a latest unstable version are defined.
      $major_stable		="3";
-     $minor_stable		="1";
+     $minor_stable		="2";
      $latest_stable             = $major_stable.".".$minor_stable;
      $latest_stable_win		= $latest_stable;
-     $latest_stable_win         = $latest_stable."-2";
-     $latest_stable_mac_intel   = $latest_stable."-3";
+#     $latest_stable_win         = $latest_stable."-2";
+     $latest_stable_mac_intel   = $latest_stable."-1";
 // Change this when there has been a re-tag to add the re-tag letter,
 // e.g. if one retagged 2.6.13 to 2.6.13a, make $tarball = $latest_stable."a".
-     $tarball                   = $latest_stable."-1";
+     $tarball                   = $latest_stable;
 
      $major_latest_unstable		="3";
      $minor_latest_unstable		="900";
