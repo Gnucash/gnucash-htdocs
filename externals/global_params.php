e337b43a368ed1d1d6cb2c45b9b5d2e66287278d
John Ralls <jralls@ceridwen.us>
2019-06-30 16:02:17 -0700

    Announce Release of GnuCash 3.6

--- externals/global_params.php
+++ externals/global_params.php
@@ -5,10 +5,10 @@
      $major_stable		="3";
      // FIXME: on major_stable 4 copy in download.phtml into the historical
      // section: $latest_stable_xp_vista_version and $latest_stable_mac_ppc
-     $minor_stable		="5";
+     $minor_stable		="6";
      $latest_stable             = $major_stable.".".$minor_stable;
      $latest_stable_win		= $latest_stable;
-     $latest_stable_win         = $latest_stable."-1";
+     // $latest_stable_win         = $latest_stable."-1";
      $latest_stable_mac_intel   = $latest_stable."-1";
 // Change this when there has been a re-tag to add the re-tag letter,
 // e.g. if one retagged 2.6.13 to 2.6.13a, make $tarball = $latest_stable."a".
