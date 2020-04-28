ce9d5f0cf3bad9ee2fba96a089d36e232834cb9c
John Ralls <jralls@ceridwen.us>
2020-04-27 20:51:20 -0700

    Don't override the unstable release value.

--- externals/global_params.php
+++ externals/global_params.php
@@ -17,8 +17,8 @@
      $major_latest_unstable		="3";
      $minor_latest_unstable		="902";
      $latest_unstable           = $major_latest_unstable.".".$minor_latest_unstable;
-     // There's currently no unstable release being worked on, so disable it
-     $latest_unstable           = True;
+// When there's an unstable release going on uncomment the line setting it to false.
+//     $latest_unstable           = False;
      $latest_unstable_win       = $latest_unstable;
      $latest_unstable_mac_intel = $latest_unstable."-1";
 
